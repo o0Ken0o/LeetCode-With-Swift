0b2b29eec7b40275cc7879dc274528d18ffa889e
Kam Siu <kamheisiu@kams-macbook-pro.local>
2016-12-28 12:52:12 +0800

    Initial Commit

---
+++ Algorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by Kam Hei Siu on 28/12/2016.
+//  Copyright © 2016 Kam Hei Siu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
