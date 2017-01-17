b6b032efa494429c8328bfb9cee49a778737f0c4
Ken Siu <kensiu20042000@gmail.com>
2017-01-17 11:26:15 +0800

    add a func to perform prime factorization

--- Algorithm/NumberTheory.swift
+++ Algorithm/NumberTheory.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 /*
-
+    find the greatest common divisor between a and b
  */
 func gcd(_ a: Int, _ b: Int) -> Int {
     let r = a % b
@@ -19,5 +19,52 @@ func gcd(_ a: Int, _ b: Int) -> Int {
     } else {
         return gcd(b, r)
     }
+}
+
+/*
+    get the prime factorization of n in the form of dictionary
+    where each key value pairs represents a prime factor and the corresponding power
+ */
+func factorize(_ n: Int) -> [Int: Int] {
+    
+    if n == 1 {
+        return [Int: Int]()
+    }
+    
+    let maxDivisor = Int(sqrt(Double(n)))
+    var factorDict = [Int: Int]()
+    var num = n
+    
+    if maxDivisor > 1 {
+        for i in 2...maxDivisor {
+            if num % i == 0 {
+                num /= i
+                factorDict = combineTwoDict(factorDict, combineTwoDict(factorize(num), factorize(i)))
+                break
+            }
+        }
+        
+        if num == n {
+            factorDict[num] = 1
+        }
+    } else {
+        factorDict[num] = 1
+    }
+    
+    return factorDict
+}
+
+func combineTwoDict(_ a: [Int: Int], _ b: [Int: Int]) -> [Int: Int] {
+    var resultDict = a
+    
+    for (key, value) in b {
+        if let oldValue = resultDict[key] {
+            resultDict[key] = oldValue + value
+        } else {
+            resultDict[key] = value
+        }
+    }
+    
+    return resultDict
 }
 
